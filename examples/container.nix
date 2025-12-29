22efb98265031367af74aaf85bbc425a2fb28984
Luke Bailey <baileylu@tcd.ie>
2025-12-29 18:03:22 +0000

    Test container generator

---
+++ examples/container.nix
@@ -0,0 +1,22 @@
+# This package seems to be used as the testbed for development of Modular Services in `nixpkgs`
+
+{
+  perSystem =
+    { pkgs, self', ... }:
+    {
+      checks.container = self'.packages.nimi.mkContainerImage {
+        services."ghostunnel-plain-old" = {
+          imports = [ pkgs.ghostunnel.services.default ];
+          ghostunnel = {
+            listen = "0.0.0.0:443";
+            cert = "/root/service-cert.pem";
+            key = "/root/service-key.pem";
+            disableAuthentication = true;
+            target = "backend:80";
+            unsafeTarget = true;
+          };
+        };
+        settings.restart.mode = "up-to-count";
+      };
+    };
+}
