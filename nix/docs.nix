47639d60381f64481a0b527c624e209c610f633c
Luke Bailey <baileylu@tcd.ie>
2025-12-29 16:53:48 +0000

    Further customize docs generation

--- nix/docs.nix
+++ nix/docs.nix
@@ -1,14 +1,33 @@
 { self, ... }:
 {
   perSystem =
-    { inputs', ... }:
+    { inputs', pkgs, ... }:
     {
-      packages.docs = inputs'.ndg.packages.ndg-builder.override {
-        title = "Nimi";
+      packages.docs =
+        let
+          moduleOpts = pkgs.lib.evalModules {
+            modules = [ self.modules.nimi.default ];
+          };
 
-        rawModules = [
-          self.modules.nimi.default
-        ];
-      };
+          moduleOptsDoc = pkgs.nixosOptionsDoc {
+            options = moduleOpts;
+          };
+        in
+        pkgs.runCommandLocal "nimi-docs"
+          {
+            nativeBuildInputs = [ inputs'.ndg.packages.default ];
+          }
+          ''
+            mkdir -p "$out/share/nimi/docs"
+
+            ndg html \
+              --input-dir "${self}/docs" \
+              --output-dir "$out/share/nimi/docs" \
+              --title "Nimi Documentation" \
+              --module-options ${moduleOptsDoc.optionsJSON}/share/doc/nixos/options.json \
+              --jobs $NIX_BUILD_CORES \
+              --generate-search \
+              --highlight-code
+          '';
     };
 }
