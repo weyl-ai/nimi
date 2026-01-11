dbe50c3b8421a8a99e5041233c1926c835506bda
Luke Bailey <baileylu@tcd.ie>
2026-01-11 15:57:12 +0000

    WIP: non flake setup

--- default.nix
+++ default.nix
@@ -1,12 +1,9 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    nodeName = lock.nodes.root.inputs.flake-compat;
-  in
-  fetchTarball {
-    url =
-      lock.nodes.${nodeName}.locked.url
-        or "https://github.com/NixOS/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
-    sha256 = lock.nodes.${nodeName}.locked.narHash;
-  }
-) { src = ./.; }).defaultNix
+{
+  pkgs ? import <nixpkgs> { },
+  nix2container ? import <nix2container> { },
+}:
+
+rec {
+  nimi = pkgs.callPackage ./nix/package.nix { inherit nix2container; };
+  default = nimi;
+}
