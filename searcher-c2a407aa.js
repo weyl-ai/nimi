63f24e5e2caf1376fe360df93c00a47b0d943c1b
baileylu121 <156000062+baileylu121@users.noreply.github.com>
2026-04-14 18:38:21 +0000

    Deploying to gh-pages from @ weyl-ai/nimi@6ab31237f98bd39e2659a24db12316a37d560d32 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-967c7f6f.js',
+                path_to_root + 'searchindex-5d5ddd57.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
